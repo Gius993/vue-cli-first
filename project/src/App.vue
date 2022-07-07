bc83fadd8374a2fd7f0976b2fd4788b8cd4eb9cf
Gius993 <giuseppevidili9@gmail.com>
2022-07-07 16:40:22 +0200

    esercizio  fine

---
+++ project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TheTitle />
+  </div>
+</template>
+
+<script>
+import TheTitle from "./components/TheTitle.vue"
+
+export default {
+  name: 'App',
+  components: {
+   TheTitle
+  }
+}
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
